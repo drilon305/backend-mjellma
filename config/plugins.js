46ea7b06a7872a89e115ea9db561454f619618bb
drilon305 <drilonsulejmani305@gmail.com>
2022-06-26 19:31:09 +0200

    Installed aws-s3 upload provider and updated the configuration

--- config/plugins.js
+++ config/plugins.js
@@ -1,13 +1,23 @@
-module.exports = ({ env }) => (
-    {
-        email: {
-            provider: 'sendgrid',
+module.exports = ({ env }) => ({
+  email: {
+    provider: "sendgrid",
     providerOptions: {
-      apiKey: env('SENDGRID_API_KEY'),
-        },
-         settings: {
-      defaultFrom: 'drilonsl305@gmail.com',
-      defaultReplyTo: 'navycrack1@gmail.com',
-         },
+      apiKey: env("SENDGRID_API_KEY"),
     },
-    })
+    settings: {
+      defaultFrom: "drilonsl305@gmail.com",
+      defaultReplyTo: "navycrack1@gmail.com",
+    },
+  },
+  upload: {
+    provider: "aws-s3",
+    providerOptions: {
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: env("AWS_REGION"),
+      params: {
+        Bucket: env("AWS_BUCKET_NAME"),
+      },
+    },
+  },
+});
