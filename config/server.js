a500eb2599e85951074dbafda1cf4e1ce803d4df
Drilon <navycrack1@gmail.com>
2022-07-01 17:38:55 +0000

    updated server config nginx

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   cron: { enabled: true},
-  url: env('URL', 'http://localhost'),
+  url: env('URL', `http://localhost${port !== '80' ? ':'+port : ''}`),
  admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '065436df64af4a6840a9bb99fc26d145'),
